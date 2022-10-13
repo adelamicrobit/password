d0cde992019951c134eea8e4c753d559ff6992d9
adelamicrobit <115727559+adelamicrobit@users.noreply.github.com>
2022-10-13 17:52:15 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,18 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     entry = "" + entry + "A"
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
+input.onButtonPressed(Button.AB, function () {
     if (password == entry) {
         basic.showIcon(IconNames.Yes)
         pins.servoWritePin(AnalogPin.P0, 90)
     } else {
         basic.showIcon(IconNames.No)
     }
-    
     basic.pause(500)
     basic.clearScreen()
     entry = ""
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.B, function () {
     entry = "" + entry + "B"
 })
 let entry = ""
@@ -24,6 +20,6 @@ let password = ""
 password = "AABAA"
 entry = ""
 pins.servoWritePin(AnalogPin.P0, 0)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
+	
 })
